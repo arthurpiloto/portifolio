a074ceebd5b727da57b31f3907dfd767b40d6c2d
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-06-06 11:01:23 -0300

    animação barra de rolagem

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+const menu= document.querySelector('#navbar');
+menu.style.zIndex = "2";
+
+window.onscroll = function () {
+    let currentScroll = window.pageYOffset;
+    if (currentScroll <= 100) {
+      menu.classList.add('.uk-navbar-transparent');
+      menu.style.backgroundColor = null;
+    } else {
+      menu.style.backgroundColor = '#202020'
+    }
+}
